4ec4e9631f837ba36d0ea1a0b1531578cfb89d9b
Proteuss <proteuss2585@gmail.com>
2017-06-20 01:02:38 -0400

    FUNCIONOOOOOOOO

--- Proyecto IA/main.cpp
+++ Proyecto IA/main.cpp
@@ -14,20 +14,20 @@
 #include <fstream>
 using namespace std;
 
-struct entity{
+struct Entity{
     int id;
     int groupId;
     float space;
 };
 
-struct room{
+struct Room{
     int id;
     int floor;
     float space;
     vector<int> adjacentRooms;
 };
 
-struct constraint{
+struct Constraint{
     int id;
     int type;
     bool hardness;
@@ -35,15 +35,50 @@ struct constraint{
     int target;
 };
 
-vector<vector<bool>> CurrentSolution;
-vector<vector<bool>> FutureSolution;
-vector<entity> entities;
-vector<room> rooms;
-vector<constraint> constraints;
-vector<vector<vector<bool>>> domain;
+vector<int> solution;
+vector<vector<int>> domain;
 
-entity readEntity(string line){//Lee una entidad a partir de una linea del .txt
-    entity ent;
+//vector<vector<bool>> CurrentSolution;
+//vector<vector<bool>> FutureSolution;
+vector<Entity> entities;
+vector<Room> rooms;
+vector<Constraint> constraints;
+//vector<vector<vector<bool>>> domain;
+
+void initSolution(){
+    for(int i=0;i<entities.size();i++){
+        solution.push_back(-1);
+    }
+}
+void initDomain(){
+    for(int i=0;i<entities.size();i++){
+        vector<int> aux;
+        for(int j=0;j<rooms.size();j++){
+            aux.push_back(j);
+        }
+        domain.push_back(aux);
+    }
+}
+void showDomain(){
+    for(vector<int> vect:domain){
+        for (int i:vect) {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+void showSolution(){
+    for (int i:solution) {
+        cout << i << " ";
+    }
+}
+
+
+
+
+
+Entity readEntity(string line){//Lee una entidad a partir de una linea del .txt
+    Entity ent;
     int step=0;
     string aux="";
     bool reading=false;
@@ -88,8 +123,8 @@ entity readEntity(string line){//Lee una entidad a partir de una linea del .txt
     return ent;
 }
 
-room readRoom(string line){//Lee una habitacion a partir de una linea del .txt
-    room rom;
+Room readRoom(string line){//Lee una habitacion a partir de una linea del .txt
+    Room rom;
     int step=0;
     string aux="";
     bool reading=false;
@@ -169,8 +204,8 @@ room readRoom(string line){//Lee una habitacion a partir de una linea del .txt
     return rom;
 }
 
-constraint readConstraint(string line){//Lee una restriccion a partir de una linea del .txt
-    constraint cons;
+Constraint readConstraint(string line){//Lee una restriccion a partir de una linea del .txt
+    Constraint cons;
     int step=0;
     string aux="";
     bool reading=false;
@@ -244,7 +279,7 @@ constraint readConstraint(string line){//Lee una restriccion a partir de una lin
 }
 
 void showEntities(){//Muestra las entidades cargadas en memoria
-    vector<entity>::iterator it;
+    vector<Entity>::iterator it;
     for (it=entities.begin(); it != entities.end(); ++it){
         cout << (*it).id << " " << (*it).groupId << " " << (*it).space << "\n";
     }
@@ -253,7 +288,7 @@ void showEntities(){//Muestra las entidades cargadas en memoria
 }
 
 void showRooms(){//Muestra las habitaciones cargadas en memoria
-    vector<room>::iterator it2;
+    vector<Room>::iterator it2;
     for (it2=rooms.begin(); it2 != rooms.end(); ++it2){
         cout << (*it2).id << " " << (*it2).floor << " " << (*it2).space << " ";
         for(vector<int>::iterator it4=(*it2).adjacentRooms.begin();it4!=(*it2).adjacentRooms.end();++it4){
@@ -265,14 +300,14 @@ void showRooms(){//Muestra las habitaciones cargadas en memoria
 }
 
 void showConstraints(){//Muestra las restricciones cargadas en memoria
-    vector<constraint>::iterator it3;
+    vector<Constraint>::iterator it3;
     for (it3=constraints.begin(); it3 != constraints.end(); ++it3){
         cout << (*it3).id << " " << (*it3).type << " " << (*it3).hardness << " " << (*it3).subject << " " << (*it3).target << "\n";
     }
 }
 
-void showSolution(){//Muestra la solucion encontrada
-    for (vector<bool> vect : CurrentSolution)
+/*void showSolution(){//Muestra la solucion encontrada
+    for (vector<bool> vect : FutureSolution)
     {
         for (bool item : vect)
         {
@@ -323,6 +358,7 @@ void initializeSolutions(){//Inicializa la matriz de soluciones en puros 0
         
     }
 }
+ 
 int getRoom(int ent){//Returns the room of the entity. Returns -1 if the entity is not allocated.
     for(int i=0;i<FutureSolution[0].size();i++){
         if(FutureSolution[ent][i]==1){
@@ -330,7 +366,8 @@ int getRoom(int ent){//Returns the room of the entity. Returns -1 if the entity 
         }
     }
     return -1;
-}
+}*/
+ 
 void readInstance(string path){//Lee la instancia de datos a partir de la direccion del archivo
     string line;
     ifstream file;
@@ -372,119 +409,189 @@ void readInstance(string path){//Lee la instancia de datos a partir de la direcc
     file.close();
     return;
 }
-bool checkFutureSolution(){
-    int count;
-    for (vector<bool> ent : FutureSolution){//Check entity is only on one room
-        count=0;
-        for (bool rom : ent){
-            if(rom){
-                count++;
-            }
-            if(count>1){
-                return false;
-            }
-        }
-    }
+
+bool checkDomain(int ent, int room){//checks if the domain value is feasible
     
-    for(constraint cons: constraints){
+    for(Constraint cons: constraints){
         if(cons.type==-1){//Unused Constraint
             continue;
         }
-        else if(cons.type==0 && cons.hardness==true){//Allocation constraint
-            if(FutureSolution[cons.subject][cons.target]==0){
+        else if(cons.type==0 && cons.hardness==true && cons.subject==ent){//Allocation constraint
+            if(cons.target!=room){
                 return false;
             }
         }
-        else if(cons.type==1 && cons.hardness==true){//Non allocation constraint
-            if(FutureSolution[cons.subject][cons.target]==1){
+        else if(cons.type==1 && cons.hardness==true && cons.subject==ent){//Non allocation constraint
+            if(cons.target==room){
                 return false;
             }
         }
-        else if(cons.type==2 && cons.hardness==true){//one of constraint (No usada en el dataset)
+        else if(cons.type==2 && cons.hardness==true){//one of constraint (Not used on the dataset)
             continue;
         }
-        else if(cons.type==3 && cons.hardness==true){//capacity constraint
+        else if(cons.type==3 && cons.hardness==true && cons.subject==room){//capacity constraint
             int spaceUsed=0;
-            for(int i=0;i<FutureSolution.size();i++){
-                if(FutureSolution[i][cons.subject]==true){
+            for(int i=0;i<solution.size();i++){
+                if(solution[i]==room){
                     spaceUsed+=entities[i].space;
                 }
             }
-            if(spaceUsed>rooms[cons.subject].space){
+            if(spaceUsed>rooms[room].space){
                 return false;
             }
         }
-        else if(cons.type==4 && cons.hardness==true){//sameroom constraint
-            if(getRoom(cons.subject)!=getRoom(cons.target)){
-                return false;
-            }
-        }
-        else if(cons.type==5 && cons.hardness==true){//not same room constraint
-            if(getRoom(cons.subject)==getRoom(cons.target)){
-                return false;
-            }
-        }
-        else if(cons.type==6 && cons.hardness==true){//not sharing constraint
-            for(int i=0;i<entities.size();i++){
-                if(i==cons.subject){
-                    continue;
+        else if(cons.type==4 && cons.hardness==true && (cons.subject==ent || cons.target==ent)){//sameroom constraint
+            if(cons.subject==ent){
+                if(solution[cons.target]==-1){
+                    continue;}
+                if(solution[cons.target]!=room){
+                    return false;
                 }
-                if(getRoom(cons.subject)==getRoom(i)){
+            }
+            else if(cons.target==ent){
+                if(solution[cons.subject]==-1){
+                    continue;}
+                if(solution[cons.subject]!=room){
                     return false;
                 }
             }
         }
-        else if(cons.type==7 && cons.hardness==true){//adjacency constraint
-            bool isAdjacent=false;
-            for(int item: rooms[getRoom(cons.subject)].adjacentRooms){
-                if(item==rooms[getRoom(cons.subject)].id){
-                    isAdjacent=true;
+        else if(cons.type==5 && cons.hardness==true && (cons.subject==ent || cons.target==ent)){//not same room constraint
+            if(cons.subject==ent){
+                if(solution[cons.target]==-1){
+                    continue;}
+                if(solution[cons.target]==room){
+                    return false;
                 }
             }
+            else if(cons.target==ent){
+                if(solution[cons.subject]==-1){
+                    continue;}
+                if(solution[cons.subject]==room){
+                    return false;
+                }
+            }
+        }
+        else if(cons.type==6 && cons.hardness==true && cons.subject==ent){//not sharing constraint
+            for(int i=0;i<entities.size();i++){
+                if(solution[i]==room){
+                    return false;
+                }
+            }
+        }
+        else if(cons.type==7 && cons.hardness==true && (cons.target==ent || cons.subject==ent)){//adjacency constraint
+            bool isAdjacent=false;
+            if(cons.subject==ent){
+                if(solution[cons.target]==-1){
+                    continue;}
+                for(int item: rooms[room].adjacentRooms){
+                    if(item==solution[cons.target]){
+                        isAdjacent=true;
+                    }
+                }
+            }
+            else if(cons.target==ent){
+                if(solution[cons.subject]==-1){
+                    continue;}
+                for(int item: rooms[room].adjacentRooms){
+                    if(item==solution[cons.subject]){
+                        isAdjacent=true;
+                    }
+                }
+            }
+
+            
             if(!isAdjacent){
                 return false;
             }
         }
-        else if(cons.type==8 && cons.hardness==true){//neaby constraint
-            if(rooms[getRoom(cons.subject)].floor!=rooms[getRoom(cons.target)].floor){
-                return false;
+        else if(cons.type==8 && cons.hardness==true && (cons.target==ent || cons.subject==ent)){//neaby constraint
+            if(cons.subject==ent){
+                if(solution[cons.target]==-1){
+                    continue;}
+                if(rooms[room].floor!=rooms[solution[cons.target]].floor){
+                    return false;
+                }
+            }
+            else if(cons.target==ent){
+                if(solution[cons.subject]==-1){
+                    continue;}
+                if(rooms[room].floor!=rooms[solution[cons.subject]].floor){
+                    return false;
+                }
+              
             }
         }
-        else if(cons.type==9 && cons.hardness==true){//away from constraint
-            if(rooms[getRoom(cons.subject)].floor==rooms[getRoom(cons.target)].floor){
-                return false;
+        else if(cons.type==9 && cons.hardness==true && (cons.target==ent || cons.subject==ent)){//away from constraint
+            if(cons.subject==ent){
+                if(solution[cons.target]==-1){
+                    continue;}
+                if(rooms[room].floor==rooms[solution[cons.target]].floor){
+                    return false;
+                }
+            }
+            else if(cons.target==ent){
+                if(solution[cons.subject]==-1){
+                    continue;}
+                if(rooms[room].floor==rooms[solution[cons.subject]].floor){
+                    return false;
+                }
+                
             }
         }
         
     }
     return true;
 }
-
+/*
 void forwardChecking(){
     for(int i=0;i<entities.size();i++){
         for(int j=0;j<rooms.size();j++){
             vector<bool>::iterator it;
             for(it=domain[i][j].begin();it!=domain[i][j].end();++it){
                 FutureSolution[i][j]=*it;
-                if(!checkFutureSolution()){
+                if(!checkDomain(i,j)){
                     domain[i][j].erase(it);
+                    --it;
                 }
             }
             if(domain[i][j].size()==0){
                 cout << "backtrack";
                 return;
             }
-            CurrentSolution[i][j]=FutureSolution[i][j]=domain[i][j][0];
+            FutureSolution[i][j]=domain[i][j][0];
         }
     }
 }
+ */
+
+void forwardChecking(){
+    long unsigned int aux;
+    for(int i=0;i<domain.size();i++){
+        aux=domain[i].size();
+        vector<int>::iterator it=domain[i].begin();
+        for(int j=0;j<aux;j++){
+            if(!checkDomain(i, j)){
+                domain[i].erase(it);
+                continue;
+            }
+            it++;
+            
+            
+        }
+        if(domain[i].size()==0){
+            cout << "Backtracking";
+        }
+        solution[i]=domain[i][0];
+    }
+}
+
 int main(int argc, const char * argv[]) {
-    readInstance("/Users/carlos/Downloads/OSAP-Instancias/nott_data/nott1d.txt");
-    initializeSolutions();
-    FutureSolution=CurrentSolution;
-    initializeDomain();
+    readInstance("/Users/carlos/Downloads/OSAP-Instancias/nott_data/toy.txt");
+    initSolution();
+    initDomain();
     forwardChecking();
     showDomain();
-    //showSolution();
+    showSolution();
     return 0;
 }
